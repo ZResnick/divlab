b594e169c6f27c5bbd8d874840ff4e25c68881c0
Joonho Han <joonhojhan@gmail.com>
2019-08-16 15:46:31 -0400

    fix: fixes error of moving images to divs

--- src/components/Droppable/index.js
+++ src/components/Droppable/index.js
@@ -2,37 +2,41 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Droppable extends Component {
-  state = {};
+	state = {};
 
-  drop = e => {
-    e.preventDefault();
-    const data = e.dataTransfer.getData('transfer');
-    e.target.appendChild(document.getElementById(data));
-    // console.log('hello');
-  };
+	drop = e => {
+		e.preventDefault();
+		const data = e.dataTransfer.getData('transfer');
+		// const element = document.getElementById(data);
+		if (data) {
+			e.target.appendChild(document.getElementById(data));
+		}
 
-  allowDrop = e => {
-    e.preventDefault();
-  };
+		// console.log('hello');
+	};
 
-  render() {
-    return (
-      <div
-        id={this.props.id}
-        onDrop={this.drop}
-        onDragOver={this.allowDrop}
-        style={this.props.style}
-      >
-        {this.props.children}
-      </div>
-    );
-  }
+	allowDrop = e => {
+		e.preventDefault();
+	};
+
+	render() {
+		return (
+			<div
+				id={this.props.id}
+				onDrop={this.drop}
+				onDragOver={this.allowDrop}
+				style={this.props.style}
+			>
+				{this.props.children}
+			</div>
+		);
+	}
 }
 
 Droppable.propTypes = {
-  id: PropTypes.string,
-  style: PropTypes.object,
-  children: PropTypes.node
+	id: PropTypes.string,
+	style: PropTypes.object,
+	children: PropTypes.node,
 };
 
 export default Droppable;
