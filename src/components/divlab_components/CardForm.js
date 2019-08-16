2bb5f8462516c24f8fe4d563108e9691426979e4
Joonho Han <joonhojhan@gmail.com>
2019-08-16 15:21:30 -0400

    fix: adds all components to sidebar

--- src/components/divlab_components/CardForm.js
+++ src/components/divlab_components/CardForm.js
@@ -3,70 +3,72 @@ import CardComponent from './CardComponent';
 import { Button, Form } from 'semantic-ui-react';
 
 export default class CardForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: '',
-      description: '',
-      id: '',
-      edit: false,
-    };
-    this.switchEdit = this.switchEdit.bind(this);
-  }
+	constructor() {
+		super();
+		this.state = {
+			name: '',
+			description: '',
+			id: '',
+			edit: false,
+		};
+		this.switchEdit = this.switchEdit.bind(this);
+	}
 
-  switchEdit(evt) {
-    evt.preventDefault();
-    console.log(this.state.edit);
-    this.setState({
-      edit: !this.state.edit,
-    });
-  }
+	switchEdit(evt) {
+		evt.preventDefault();
+		console.log(this.state.edit);
+		this.setState({
+			edit: !this.state.edit,
+		});
+	}
 
-  handleChange = evt => {
-    this.setState({
-      [evt.target.name]: evt.target.value,
-    });
-    console.log(this.state.content);
-  };
+	handleChange = evt => {
+		this.setState({
+			[evt.target.name]: evt.target.value,
+		});
+		console.log(this.state.content);
+	};
 
-  render() {
-    return this.state.edit ? (
-      <div>
-        <Form onSubmit={this.switchEdit}>
-          <Form.Input
-            label="div ID"
-            type="text"
-            name="id"
-            value={this.state.id}
-            placeholder="<div> #ID"
-            onChange={this.handleChange}
-          />
-          <Form.Input
-            label="Name"
-            type="text"
-            name="name"
-            value={this.state.name}
-            placeholder="Name"
-            onChange={this.handleChange}
-          />
-          <Form.Input
-            label="Description"
-            type="text"
-            name="description"
-            value={this.state.description}
-            placeholder="Description"
-            onChange={this.handleChange}
-          />
-          <Button type="submit">Preview</Button>
-        </Form>
-      </div>
-    ) : (
-      <div id={this.state.id && this.state.id}>
-        <CardComponent info={this.state} />
-        <Button className="edit-button-on" onClick={this.switchEdit} width={6}>
-          Edit
-        </Button>
-      </div>
-    );
-  }
+	render() {
+		return this.state.edit ? (
+			<div>
+				<Form onSubmit={this.switchEdit}>
+					<Form.Input
+						label="div ID"
+						type="text"
+						name="id"
+						value={this.state.id}
+						placeholder="<div> #ID"
+						onChange={this.handleChange}
+					/>
+					<Form.Input
+						label="Name"
+						type="text"
+						name="name"
+						value={this.state.name}
+						placeholder="Name"
+						onChange={this.handleChange}
+					/>
+					<Form.Input
+						label="Description"
+						type="text"
+						name="description"
+						value={this.state.description}
+						placeholder="Description"
+						onChange={this.handleChange}
+					/>
+					<Button className="edit-button-on" type="submit">
+						Preview
+					</Button>
+				</Form>
+			</div>
+		) : (
+			<div id={this.state.id && this.state.id}>
+				<CardComponent info={this.state} />
+				<Button className="edit-button-on" onClick={this.switchEdit} width={6}>
+					Edit
+				</Button>
+			</div>
+		);
+	}
 }
--- src/components/divlab_components/ParagraphForm.js
+++ src/components/divlab_components/ParagraphForm.js
@@ -3,61 +3,63 @@ import ParagraphComponent from './ParagraphComponent';
 import { Button, Form } from 'semantic-ui-react';
 
 export default class ParagraphForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      content: '',
-      id: '',
-      edit: true,
-    };
-    this.switchEdit = this.switchEdit.bind(this);
-  }
+	constructor() {
+		super();
+		this.state = {
+			content: '',
+			id: '',
+			edit: true,
+		};
+		this.switchEdit = this.switchEdit.bind(this);
+	}
 
-  switchEdit(evt) {
-    evt.preventDefault();
-    console.log(this.state.edit);
-    this.setState({
-      edit: !this.state.edit,
-    });
-  }
+	switchEdit(evt) {
+		evt.preventDefault();
+		console.log(this.state.edit);
+		this.setState({
+			edit: !this.state.edit,
+		});
+	}
 
-  handleChange = evt => {
-    this.setState({
-      [evt.target.name]: evt.target.value,
-    });
-    console.log(this.state.content);
-  };
+	handleChange = evt => {
+		this.setState({
+			[evt.target.name]: evt.target.value,
+		});
+		console.log(this.state.content);
+	};
 
-  render() {
-    return this.state.edit ? (
-      <div>
-        <Form onSubmit={this.switchEdit}>
-          <Form.Input
-            label="div ID"
-            type="text"
-            name="id"
-            value={this.state.id}
-            placeholder="<div> #ID"
-            onChange={this.handleChange}
-          />
-          <Form.Input
-            label="Paragraph Content"
-            type="text"
-            name="content"
-            value={this.state.content}
-            placeholder="Content"
-            onChange={this.handleChange}
-          />
-          <Button type="submit">Preview</Button>
-        </Form>
-      </div>
-    ) : (
-      <div id={this.state.id && this.state.id}>
-        <ParagraphComponent info={this.state} />
-        <Button className="edit-button-on" onClick={this.switchEdit} width={6}>
-          Edit
-        </Button>
-      </div>
-    );
-  }
+	render() {
+		return this.state.edit ? (
+			<div>
+				<Form onSubmit={this.switchEdit}>
+					<Form.Input
+						label="div ID"
+						type="text"
+						name="id"
+						value={this.state.id}
+						placeholder="<div> #ID"
+						onChange={this.handleChange}
+					/>
+					<Form.Input
+						label="Paragraph Content"
+						type="text"
+						name="content"
+						value={this.state.content}
+						placeholder="Content"
+						onChange={this.handleChange}
+					/>
+					<Button className="edit-button-on" type="submit">
+						Preview
+					</Button>
+				</Form>
+			</div>
+		) : (
+			<div id={this.state.id && this.state.id}>
+				<ParagraphComponent info={this.state} />
+				<Button className="edit-button-on" onClick={this.switchEdit} width={6}>
+					Edit
+				</Button>
+			</div>
+		);
+	}
 }
