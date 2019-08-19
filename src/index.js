9965a6627cb98b66d0958371c57cba98870d9885
Zach Resnick <zachary.l.resnick@gmail.com>
2019-08-19 14:54:20 -0400

    Firebase Redux set-up

--- src/index.js
+++ src/index.js
@@ -5,12 +5,35 @@ import history from './history';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './store/rootReducer';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import firebaseConfig from './config/firebase';
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(firebaseConfig),
+    reactReduxFirebase(firebaseConfig)
+  )
+);
+
+//EXPLAINING THE ABOVE CREATESTORE
+//that withExtraArgument({ getFirebase, getFirestore })) allows you to add getFirebase or getFirestore to arguments passed to each thunk, see the reducers.
+//To get these to work though, we must make them aware of our firestore using a store enhancer...
+//Import compose from redux and put the applymiddelware into that as composes first argument, import reduxFirestore and reactReduxFirebase add these to compose as well, then lastly pass the firestore config to those so that when we use getFirestore and getFirebase, they know about the store.
 
 ReactDOM.render(
-	<Router history={history}>
-		<App />
-	</Router>,
-	document.getElementById('root')
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
