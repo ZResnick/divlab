7ff0f0a689d3a5def96664f640ee706b798e138c
Joonho Han <joonhojhan@gmail.com>
2019-08-23 16:30:09 -0400

    adds headshot regex

--- src/components/divlab_components/HeadshotComponent.js
+++ src/components/divlab_components/HeadshotComponent.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
 export default function HeadshotComponent(props) {
-  let { imageUrl } = props.info;
+	let { imageUrl } = props.info;
 
-  let divStyle = {
-    height: '100%',
-    width: '100%',
-    margin: '0px',
-    display: 'block'
-  };
+	let divStyle = {
+		height: '100%',
+		width: '100%',
+		margin: '0px',
+		display: 'block',
+	};
 
-  return imageUrl.length ? (
-    <div style={divStyle}>
-      <img alt="" src={imageUrl} />
-    </div>
-  ) : (
-    <img alt="" src="images/HeadshotExample.png" />
-  );
+	return imageUrl.length ? (
+		<div style={divStyle} name="HeadshotComponent">
+			<span dangerouslySetInnerHTML={{ __html: '<!-- HeadshotSrcStart -->' }} />
+			<img alt="" src={imageUrl} />
+			<span dangerouslySetInnerHTML={{ __html: '<!-- HeadshotSrcEnd -->' }} />
+		</div>
+	) : (
+		<img alt="" src="images/HeadshotExample.png" />
+	);
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,44 +4,58 @@ import ReactDOM from 'react-dom';
 import ParagraphForm from '../components/divlab_components/ParagraphForm';
 
 export function setLocalStorageHTML() {
-  console.log(document.querySelector('.react-grid-layout'));
+	console.log(document.querySelector('.react-grid-layout'));
 
-  if (document.querySelector('.react-grid-layout')) {
-    localStorage.setItem(
-      'canvas',
-      document.querySelector('.react-grid-layout').innerHTML
-    );
-    console.log('setLocalStorageHTML ran succesfully!');
-  }
+	if (document.querySelector('.react-grid-layout')) {
+		localStorage.setItem(
+			'canvas',
+			document.querySelector('.react-grid-layout').innerHTML
+		);
+		console.log('setLocalStorageHTML ran succesfully!');
+	}
 }
 
 export function setHTML() {
-  let canvas = document.querySelector('.react-grid-layout');
-  return canvas.innerHTML;
+	let canvas = document.querySelector('.react-grid-layout');
+	return canvas.innerHTML;
 }
 
 export function reactDomRender(state) {
-  if (document.querySelector('.react-grid-layout')) {
-    ReactDOM.render(
-      state.usedComponents[0],
-      document.querySelector('.react-grid-layout')
-    );
-  }
+	if (document.querySelector('.react-grid-layout')) {
+		ReactDOM.render(
+			state.usedComponents[0],
+			document.querySelector('.react-grid-layout')
+		);
+	}
 }
 
 export function paragraphContentParser(canvas) {
-  let paragraphRegex = /(?<=<!-- ParagraphContentStart --><\/p><p>)(.*?)(?=<\/p><p><!-- ParagraphContentEnd -->)/g;
-  let output = canvas.match(paragraphRegex);
-  let output1 = [...canvas.matchAll(paragraphRegex)];
-  console.log(output1);
-  // console.log('FOUND>', output1[2][0], 'INDEX>', output1[2]['index']);
-  // let array;
-  // while ((array = paragraphRegex.exec(canvas)) !== null) {
-  // 	console.log(
-  // 		`Found ${array[0]}. Next starts at ${paragraphRegex.lastIndex}`
-  // 	);
-  // }
-  return output;
+	let paragraphRegex = /(?<=<!-- ParagraphContentStart --><\/p><p>)(.*?)(?=<\/p><p><!-- ParagraphContentEnd -->)/g;
+	let output = canvas.match(paragraphRegex);
+	let output1 = [...canvas.matchAll(paragraphRegex)];
+	console.log(output1);
+	// console.log('FOUND>', output1[2][0], 'INDEX>', output1[2]['index']);
+	// let array;
+	// while ((array = paragraphRegex.exec(canvas)) !== null) {
+	// 	console.log(
+	// 		`Found ${array[0]}. Next starts at ${paragraphRegex.lastIndex}`
+	// 	);
+	// }
+	return output;
+}
+export function headshotParser(canvas) {
+	let headshotRegex = /(?<=<!-- HeadshotSrcStart --><\/span><img alt="" src=")(.*?)(?="><span><!-- HeadshotSrcEnd -->)/g;
+	let output = canvas.match(headshotRegex);
+	let output1 = [...canvas.matchAll(headshotRegex)];
+	console.log(output1);
+	// console.log('FOUND>', output1[2][0], 'INDEX>', output1[2]['index']);
+	// let array;
+	// while ((array = paragraphRegex.exec(canvas)) !== null) {
+	// 	console.log(
+	// 		`Found ${array[0]}. Next starts at ${paragraphRegex.lastIndex}`
+	// 	);
+	// }
+	return output;
 }
 
 /*
@@ -57,3 +71,5 @@ That function is what needs to be called on line 98 of divlab2.
 
 Inside the for loop is where we'll put the switch statement that will search by type nd inject the props
 */
+
+export function regexer(canvas) {}
