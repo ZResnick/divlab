43124c3f4d39873ee724fec171d2b12b1887c092
Joonho Han <joonhojhan@gmail.com>
2019-08-28 15:16:21 -0400

    adds photo of Elliot to about us section

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,75 +2,71 @@ import { Card, Image } from 'semantic-ui-react';
 import React from 'react';
 
 export default function Home() {
-  return (
-    <div>
-      <div id="homepage">
-        <h1 className="animate-welcome">{'<divlab />'}</h1>
-        <h3 className="animate-caption">THE FUTURE OF WEB DESIGN</h3>
-      </div>
+	return (
+		<div>
+			<div id="homepage">
+				<h1 className="animate-welcome">{'<divlab />'}</h1>
+				<h3 className="animate-caption">THE FUTURE OF WEB DESIGN</h3>
+			</div>
 
-      <div>
-        <h3 className="team">THE TEAM BEHIND THE IDEA</h3>
-        <center>
-          <div id="teamBar" />
-        </center>
-        <div id="teamCards">
-          <Card.Group centered="true">
-            <Card raised="true" className="teamSingleCards">
-              <Image src="images/ZacharyResnick.png" wrapped ui={false} />
-              <Card.Content style={{ height: '73.16px' }}>
-                <Card.Header>
-                  <a href="https://www.linkedin.com/in/zachresnick1/">
-                    <span className="newProjectText">ZACHARY RESNICK</span>
-                  </a>
-                  <p className="captionText">SOFTWARE ENGINEER</p>
-                </Card.Header>
-              </Card.Content>
-            </Card>
-            <Card raised="true" className="teamSingleCards" id="profileCards">
-              <Image src="images/JoonhoHan.png" wrapped ui={false} />
-              <Card.Content style={{ height: '73.16px' }}>
-                <Card.Header>
-                  <a href="https://www.linkedin.com/in/joonhojhan/">
-                    <span className="newProjectText">JOONHO HAN</span>
-                  </a>
-                  <p className="captionText">SOFTWARE ENGINEER</p>
-                </Card.Header>
-              </Card.Content>
-            </Card>
-            <Card raised="true" className="teamSingleCards">
-              <Image
-                src="https://react.semantic-ui.com/images/avatar/large/daniel.jpg"
-                wrapped
-                ui={false}
-              />
-              <Card.Content style={{ height: '73.16px' }}>
-                <Card.Header>
-                  <a href="https://www.linkedin.com/in/elliot-gonzalez-4b18534a/">
-                    <span className="newProjectText">ELLIOT GONZALEZ</span>
-                  </a>
-                  <p className="captionText">SOFTWARE ENGINEER</p>
-                </Card.Header>
-              </Card.Content>
-            </Card>
-          </Card.Group>
-        </div>
-      </div>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-    </div>
-  );
+			<div>
+				<h3 className="team">THE TEAM BEHIND THE IDEA</h3>
+				<center>
+					<div id="teamBar" />
+				</center>
+				<div id="teamCards">
+					<Card.Group centered="true">
+						<Card raised="true" className="teamSingleCards">
+							<Image src="images/ZacharyResnick.png" wrapped ui={false} />
+							<Card.Content style={{ height: '73.16px' }}>
+								<Card.Header>
+									<a href="https://www.linkedin.com/in/zachresnick1/">
+										<span className="newProjectText">ZACHARY RESNICK</span>
+									</a>
+									<p className="captionText">SOFTWARE ENGINEER</p>
+								</Card.Header>
+							</Card.Content>
+						</Card>
+						<Card raised="true" className="teamSingleCards" id="profileCards">
+							<Image src="images/JoonhoHan.png" wrapped ui={false} />
+							<Card.Content style={{ height: '73.16px' }}>
+								<Card.Header>
+									<a href="https://www.linkedin.com/in/joonhojhan/">
+										<span className="newProjectText">JOONHO HAN</span>
+									</a>
+									<p className="captionText">SOFTWARE ENGINEER</p>
+								</Card.Header>
+							</Card.Content>
+						</Card>
+						<Card raised="true" className="teamSingleCards">
+							<Image src="images/ElliotGonzalez.png" wrapped ui={false} />
+							<Card.Content style={{ height: '73.16px' }}>
+								<Card.Header>
+									<a href="https://www.linkedin.com/in/elliot-gonzalez-4b18534a/">
+										<span className="newProjectText">ELLIOT GONZALEZ</span>
+									</a>
+									<p className="captionText">SOFTWARE ENGINEER</p>
+								</Card.Header>
+							</Card.Content>
+						</Card>
+					</Card.Group>
+				</div>
+			</div>
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+		</div>
+	);
 }
