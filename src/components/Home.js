8b180b78ebc6822f31878cc1ff26f8ec7ab3cb74
Zach Resnick <zachary.l.resnick@gmail.com>
2019-08-28 16:34:10 -0400

    Changes home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,16 @@ import { Card, Image } from 'semantic-ui-react';
 import React from 'react';
 
 export default function Home() {
-	return (
-		<div>
-			<div id="homepage">
-				<h1 className="animate-welcome">{'<divlab />'}</h1>
-				<h3 className="animate-caption">THE FUTURE OF WEB DESIGN</h3>
-			</div>
-		</div>
-	);
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div id="homepage">
+        <h1 className="animate-welcome">{'<divlab />'}</h1>
+        <h3 className="animate-caption">THE FUTURE OF WEB DESIGN</h3>
+      </div>
+
+      {/* <div id="rightHomepage">
+        <p>Hello</p>
+      </div> */}
+    </div>
+  );
 }
